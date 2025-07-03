432877636081c5e92376d63aa9b9cdeba45caf69
krosengr4 <rosenkev4@gmail.com>
2025-07-03 13:49:32 -0500

    Batman

---
+++ SixShooterStories/src/main/java/com/pluralsight/SixShooterStories/SixShooterStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.SixShooterStories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixShooterStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixShooterStoriesApplication.class, args);
+	}
+
+}
---
+++ SixShooterStories/src/test/java/com/pluralsight/SixShooterStories/SixShooterStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.SixShooterStories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixShooterStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
