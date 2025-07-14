90b0bbe1521dc068027b7dfaf2de934bef238ee6
krosengr4 <rosenkev4@gmail.com>
2025-07-14 00:21:20 -0500

    Created controller to get and return all stories

--- SixShooterStories/src/main/java/com/pluralsight/SixShooterStories/controllers/StoryController.java
+++ SixShooterStories/src/main/java/com/pluralsight/SixShooterStories/controllers/StoryController.java
@@ -1,10 +1,16 @@
 package com.pluralsight.SixShooterStories.controllers;
 
 import com.pluralsight.SixShooterStories.data.StoryDao;
+import com.pluralsight.SixShooterStories.models.Story;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -16,6 +22,15 @@ public class StoryController {
 	@Autowired
 	public StoryController(StoryDao storyDao) {
 		this.storyDao = storyDao;
+	}
+
+	@GetMapping("")
+	public List<Story> getAllStories() {
+		try {
+			return storyDao.getAll();
+		} catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "The server could not get that...");
+		}
 	}
 
 
