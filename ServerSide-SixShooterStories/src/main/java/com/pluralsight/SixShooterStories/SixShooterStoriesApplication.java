d055c14a9f9bac5194461fc69cdd7b34c08c2371
krosengr4 <rosenkev4@gmail.com>
2025-07-23 04:35:42 -0500

    Added link to print out whenever the server side runs

--- ServerSide-SixShooterStories/src/main/java/com/pluralsight/SixShooterStories/SixShooterStoriesApplication.java
+++ ServerSide-SixShooterStories/src/main/java/com/pluralsight/SixShooterStories/SixShooterStoriesApplication.java
@@ -8,6 +8,7 @@ public class SixShooterStoriesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SixShooterStoriesApplication.class, args);
+		System.out.println("http://localhost:63342/ServerSide-SixShooterStories/ClientSide-SixShooterStories/index.html?_ijt=auu1v4kt65f6hieg58gqafftd7&_ij_reload=RELOAD_ON_SAVE");
 	}
 
 }
